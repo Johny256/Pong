b180aae2b508d910c4642fa3390ccc4385b6e110
Johny <jecf526@gmail.com>
2018-04-28 12:58:07 -0500

    add sounds

---
+++ src/com/eagle256/game/Sound.java
@@ -0,0 +1,10 @@
+package com.eagle256.game;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+
+	public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+
+}
